4e0f710c4f79a99e560c48d92646d31e7528d977
falafely <126647920+falafely@users.noreply.github.com>
2023-08-23 12:27:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ demotivator = Image.new('RGB', (1000, 1000), color='black')
 draw = ImageDraw.Draw(demotivator)
 draw.rectangle((170, 170, 830, 830), fill='black', outline=(255, 255, 255), width=3)
 
-photo = Image.open('your_pict.png')  # your_pict.png - ваше изображение
+photo = Image.open('your_pict.png')  # your_pict.png - your picture
 resize_photo = photo.resize((600, 600))
 
 new_x, new_y = resize_photo.size
@@ -16,13 +16,13 @@ demotivator.paste(resize_photo, (a, b))
 
 text = 'example'
 
-font = ImageFont.truetype('/usr/share/fonts/TTF/AkaashNormal.ttf', size=50)  # заменить шрифт на свой
+font = ImageFont.truetype('/usr/share/fonts/TTF/AkaashNormal.ttf', size=50)  # replace the font with yours
 
 text_width, text_height = draw.textsize(text, font=font)
 x = (demotivator.width - text_width) // 2
 y = 900
 
-draw.text((x, y), text, font=font, fill=(255, 255, 255))  # выставить свои координаты
+draw.text((x, y), text, font=font, fill=(255, 255, 255))  # replace (x, y) with your coordinates
 
 demotivator.show()
 demotivator.save('demotivator.png')
